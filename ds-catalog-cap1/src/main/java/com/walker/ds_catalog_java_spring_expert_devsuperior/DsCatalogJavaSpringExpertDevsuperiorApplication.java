877ccfa08bf36444c897b9d646b71cb59acf4d18
Walker <walker.esteves.negrao@gmail.com>
2026-01-16 08:40:03 -0300

    Criação do repositório

---
+++ ds-catalog-cap1/src/main/java/com/walker/ds_catalog_java_spring_expert_devsuperior/DsCatalogJavaSpringExpertDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.walker.ds_catalog_java_spring_expert_devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogJavaSpringExpertDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogJavaSpringExpertDevsuperiorApplication.class, args);
+	}
+
+}
