fcaf6fc641f436a57a353e335e15b627eca51be8
Walker <walker.esteves.negrao@gmail.com>
2026-01-16 12:49:51 -0300

    Atualização

---
+++ ds-catalog-cap1/src/main/java/com/walker/ds_catalog_java_spring_expert_devsuperior/model/domain/Category.java
@@ -0,0 +1,9 @@
+package com.walker.ds_catalog_java_spring_expert_devsuperior.model.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+}
